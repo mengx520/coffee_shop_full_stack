db8a3084617612d73cb9b9eb6d38225a0a5bde63
mengx520 <mengx520@gmail.com>
2021-05-27 19:43:19 -0700

    Fix environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'xiaohan.us.auth0.com', // the auth0 domain prefix
+    url: 'xiaohan.usgit', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'EuxISRtO9SnB6OlAhFp5fj0zctbJiinD', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
